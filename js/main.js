7595c78b488646d9ae7c6576c8462d2413381305
Kevin Whittaker <kevinlwhittaker@gmail.com>
2016-05-18 12:51:03 +0200

    adding skybox, adding prototype gravity

--- js/main.js
+++ js/main.js
@@ -86,8 +86,31 @@ function initBottom(){
 	var plane = new THREE.Mesh( geometry, material );
 
 	plane.rotation.x += (deg2rad(90));
-	plane.position.y = -100;
+	plane.position.y = -50;
 	SCENE.add( plane );
+}
+
+function initSkyBox(){
+	var path = "img/skybox/";
+	var format = '.jpg';
+	var urls = [
+		path + 'px' + format, path + 'nx' + format,
+		path + 'py' + format, path + 'ny' + format,
+		path + 'pz' + format, path + 'nz' + format
+	];
+
+	var textureCube = new THREE.CubeTextureLoader().load( urls );
+	var shader = THREE.ShaderLib[ "cube" ];
+	shader.uniforms[ "tCube" ].value = textureCube;
+	var material = new THREE.ShaderMaterial( {
+		fragmentShader: shader.fragmentShader,
+		vertexShader: shader.vertexShader,
+		uniforms: shader.uniforms,
+		depthWrite: false,
+		side: THREE.BackSide
+	} ),
+	mesh = new THREE.Mesh( new THREE.BoxGeometry( 1500, 1500, 1500 ), material );
+	SCENE.add( mesh );
 }
 
 function addSphere(){
@@ -95,16 +118,39 @@ function addSphere(){
 		color: 0xCC0000
 	});
 
-	var radius = 50;
+	var radius = 10;
 	var segments = 32;
 	var rings = 32;
 
 	var sphereGeometry =  new THREE.SphereGeometry(	radius,	segments, rings);
 	sphere = new THREE.Mesh( sphereGeometry, sphereMaterial );
+	sphere.userData = {"yVelocity": 0};
 	SCENE.add(sphere);
 }
 
+
+function floating(obj){
+	var objPosition = new THREE.Vector3(obj.position.x, obj.position.y, obj.position.z);
+	var raycaster = new THREE.Raycaster();
+
+	raycaster.set( objPosition, new THREE.Vector3(0,-1,0) );	
+	var under = raycaster.intersectObjects( SCENE.children );
+	raycaster.set( objPosition, new THREE.Vector3(0,1,0) );	
+	var above = raycaster.intersectObjects( SCENE.children );
+	var yDiff = 0;
+	if(under[0].object.name == "waterSurface"){
+		//yDiff = -under[0].distance;
+		obj.userData.yVelocity -= 0.05;
+	}else if(above[0].object.name == "waterSurface"){
+		//yDiff = above[0].distance;
+		obj.userData.yVelocity += 0.02;
+	}
+	//obj.userData.yVelocity += yDiff*0.1;
+
+}
+
 function render(){
+
 	STEP += 0.07;
 
 	// pointLight.position.y = Math.sin(STEP)*300; 
@@ -117,20 +163,22 @@ function animate(){
 	requestAnimationFrame(animate);
 	//define wave origin
 	//v.z is local to the plane. due to rotation this corresponds to global y
-	var magnitude = 7.0;
-	var size = 10.0;
+	var magnitude = 3.0;
+	var size = 5.0;
 	var decay = 0.1;
 	var vLength = plane.geometry.vertices.length;
-	if (STEP < magnitude/decay){
+	//if (STEP < magnitude/decay){
 	  	for (var i = 0; i < vLength; i++) {
 		    var v = plane.geometry.vertices[i];
 		    var dist = new THREE.Vector2(v.x, v.y).sub(EPICENTER);		    
 			v.z = Math.sin(dist.length()/size - STEP) * (magnitude - STEP*decay);
 		}
 	// animate floating ball
-	var dist = new THREE.Vector2(sphere.position.x, sphere.position.z).sub(EPICENTER);		
-	sphere.position.y =  Math.sin((dist.length())/size - (STEP)) * (magnitude - STEP*decay);
-	}
+	// var dist = new THREE.Vector2(sphere.position.x, sphere.position.z).sub(EPICENTER);		
+	// sphere.position.y =  Math.sin((dist.length())/size - (STEP)) * (magnitude - STEP*decay);
+	sphere.position.y += sphere.userData.yVelocity;
+	floating(sphere);
+	//}
 
 	plane.geometry.verticesNeedUpdate = true;
 	render();
@@ -138,6 +186,7 @@ function animate(){
 
 
 var SCENE = init();
+initSkyBox();
 initCtrl();
 initSurface();
 initBottom();
